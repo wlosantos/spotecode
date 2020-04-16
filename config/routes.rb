8855b3bd557426c81bc60df97a997907949be471
Wendel Lopes <wendelopes@msn.com>
2020-04-16 16:01:28 -0300

    Endpoint finalizado

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
 
+  concern :favoritable do |option|
+    shallow do
+      post '/favorite', {to: 'favorite#create', on: :member}.merge(option)
+      delete '/favorite', {to: 'favorite#destroy', on: :member}.merge(option)
+    end
+  end
+
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :dashboard, only: :index
@@ -11,6 +18,11 @@ Rails.application.routes.draw do
       resources :search, only: :index
       resources :albums, only: :show do
         resources :recently_heards, only: :create
+      end
+      resources :favorites, only: :index
+
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type: 'song'
       end
     end
   end
